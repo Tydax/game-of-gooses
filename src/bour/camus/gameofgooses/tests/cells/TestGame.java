cfeaf5fb2af954b139afb737f84db457db28037e
Camus Tristan <camus@a16p5.fil.univ-lille1.fr>
2014-09-24 10:45:05 +0200

    Added a Testcase for the Game class.

---
+++ src/bour/camus/gameofgooses/tests/cells/TestGame.java
@@ -0,0 +1,24 @@
+package bour.camus.gameofgooses.tests.cells;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestGame {
+
+	@Test
+	public void testPlay() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsFinished() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNextPlayer() {
+		fail("Not yet implemented");
+	}
+
+}
